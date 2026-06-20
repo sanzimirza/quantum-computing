28bb2b0b8ccf66a1699e2bf1f02d79423badf543
Sanzi <156568336+sanzimirza@users.noreply.github.com>
2026-06-21 02:51:01 +0530

    minor change in Grover's Algorithm.py

--- Grover's Algorithm/Grover's Algorithm.py
+++ Grover's Algorithm/Grover's Algorithm.py
@@ -13,7 +13,7 @@ def run_and_print(qc, label):
     qc.save_statevector() # after running this circuit save a snapshot of this so that I can review this later
     result = sim.run(qc).result() #submission and fetching of the result
     sv = np.asarray(result.get_statevector())
-    #sv = result.get_statevector() # sv is a list of complex numbers (the amplitudes of the collapse states)
+    # sv is a list of complex numbers (the amplitudes of the collapse states)
     print(f"\n---{label} ---") #prints the heading lol
     print(qc.draw('text')) #draws a nice diagram for your circuit
     print("Statevector:", [round(float(x.real),3) for x in sv]) #prints the real part of all the amplitudes
